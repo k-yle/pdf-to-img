75eba427aea84a7ef9bab5fec16d77f456fd8019
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-10-04 22:02:40 +1300

    update linter

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -2,5 +2,6 @@
 global.window ||= global;
 
 // 20ms because that's what pdfjs used to do: https://github.com/mozilla/pdf.js/pull/9138/files
-global.window.requestAnimationFrame = (cb) => global.window.setTimeout(cb, 20);
+global.window.requestAnimationFrame = (callback) =>
+  global.window.setTimeout(callback, 20);
 global.window.cancelAnimationFrame = (id) => global.window.clearTimeout(id);
--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,5 +1,6 @@
-import { configureToMatchImageSnapshot } from "jest-image-snapshot";
+/* eslint-disable import/no-extraneous-dependencies */
 import { TextEncoder, TextDecoder } from "node:util";
+import { configureToMatchImageSnapshot } from "jest-image-snapshot";
 
 // need to polyfill these for the JSDom env, even on node v19 - https://github.com/jsdom/jsdom/issues/2524
 global.TextEncoder = TextEncoder;
